b974ba3c711bb63fc1e7d3db10efd15d361fa8e4
Zoren <zorenleolumosbog@gmail.com>
2023-02-09 17:23:50 +0800

    commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+   <h1>About Page</h1>
+   <p>Today’s hotels can’t get away with only having one sales channel, or relying solely on walk-ins. Between the popular booking websites and travel agencies, you have a lot of different channels that you need to manage. If your room inventory doesn’t stay in sync between all of your sales channels, you could end up overbooking, and not in a clever way. The opposite problem can occur if all of your sales channels aren’t promptly notified when a room becomes available. Another benefit of having all of your sales channels in one location, is that your staff will also have easy access to all of your reservations at once.</p> 
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+   <h1 style="text-align:center; ">Home Page</h1> 
+   
+   <div class="container-fluid" style="background-image: url({{  asset("images/library.jpg")}});">
+      <div class="container">
+         <div class="col-md-6">
+
+         </div>
+         <div class="col-md-6">
+            
+         </div>
+      </div>
+   </div>
+@endsection
